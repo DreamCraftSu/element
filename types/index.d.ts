5715bcc03c880117e21ccd148576084d1a7b2cdf
Two-Faces <anton98767@gmail.com>
2020-08-28 15:04:05 +0300

    Update project

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-export * from './element-ui'
+export * from './exarcheia-element'
 
-import * as ElementUI from './element-ui'
+import * as ElementUI from './exarcheia-element'
 export default ElementUI

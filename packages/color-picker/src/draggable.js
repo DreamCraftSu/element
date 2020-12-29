867ca8620540586665ade13545dd1e89545ac77f
Two-Faces <anton98767@gmail.com>
2020-12-30 02:58:39 +0300

    Added touch events on draggable color-picker

--- packages/color-picker/src/draggable.js
+++ packages/color-picker/src/draggable.js
@@ -3,23 +3,49 @@ let isDragging = false;
 
 export default function(element, options) {
   if (Vue.prototype.$isServer) return;
-  const moveFn = function(event) {
-    if (options.drag) {
-      options.drag(event);
+
+  // Touch
+  element.addEventListener('touchstart', function(e) {
+    if (isDragging) return;
+    document.onselectstart = function() { return false; };
+    document.ondragstart = function() { return false; };
+
+    document.addEventListener('touchmove', touchMove);
+    document.addEventListener('touchend', fouchUpFn);
+    isDragging = true;
+
+    if (options.start) {
+      options.start(getMouseGenerated(e, 'mousedown'));
     }
+  });
+
+  const getMouseGenerated = (e, type) => {
+    const touch = type === 'mouseup' ? e.changedTouches[0] : e.touches[0];
+
+    return new MouseEvent(type, {
+      clientX: touch.clientX,
+      clientY: touch.clientY
+    });
   };
-  const upFn = function(event) {
-    document.removeEventListener('mousemove', moveFn);
-    document.removeEventListener('mouseup', upFn);
-    document.onselectstart = null;
-    document.ondragstart = null;
+
+  const fouchUpFn = (e) => {
+    document.removeEventListener('touchmove', touchMove);
+    document.removeEventListener('touchend', fouchUpFn);
 
     isDragging = false;
 
     if (options.end) {
-      options.end(event);
+      options.end(getMouseGenerated(e, 'mouseup'));
     }
   };
+
+  const touchMove = (e) => {
+    if (options.drag) {
+      options.drag(getMouseGenerated(e, 'mousemove'));
+    }
+  };
+
+  // Mouse
   element.addEventListener('mousedown', function(event) {
     if (isDragging) return;
     document.onselectstart = function() { return false; };
@@ -33,4 +59,23 @@ export default function(element, options) {
       options.start(event);
     }
   });
+
+  const moveFn = function(event) {
+    if (options.drag) {
+      options.drag(event);
+    }
+  };
+
+  const upFn = function(event) {
+    document.removeEventListener('mousemove', moveFn);
+    document.removeEventListener('mouseup', upFn);
+    document.onselectstart = null;
+    document.ondragstart = null;
+
+    isDragging = false;
+
+    if (options.end) {
+      options.end(event);
+    }
+  };
 }
